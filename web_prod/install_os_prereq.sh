22365341083158f62d131f348c5c0ff5a9612645
root <root@ip-10-0-1-79.ap-south-1.compute.internal>
2020-08-12 19:45:26 +0000

    added module to install os dependencies

---
+++ web_prod/install_os_prereq.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+source ./utils.sh
+
+check_root
+
+LINUX_PREREQ=('git' 'build-essential' 'python3.7-dev' 'python3.7-venv' 'python3-pip' 'nginx' 'ufw')
+
+# Test prerequisites
+echo "Checking if required packages are installed..."
+declare -a MISSING
+
+for pkg in "${LINUX_PREREQ[@]}"
+    do
+        echo "Installing '$pkg'..."
+        apt-get -y install $pkg
+        if [ $? -ne 0 ]; then
+            echo "Error installing system package '$pkg'"
+            exit 1
+        fi
+    done
+
+
+if [ ${#MISSING[@]} -ne 0 ]; then
+    echo "Following required packages are missing, please install them first."
+    echo ${MISSING[*]}
+    exit 1
+fi
+
+echo "All required packages have been installed!"
