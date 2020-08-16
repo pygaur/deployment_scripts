8e2100760a633c2c0f41cda5d19fab85afcb128a
Prashant Gaur <prasgaur@cloudlock.com>
2020-08-17 00:27:14 +0530

    updated db files

---
+++ db_prod/install_os_prereq.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+source ./utils.sh
+
+check_root
+
+LINUX_PREREQ=('postgresql postgresql-contrib')
+
+# Test prerequisites
+echo "Checking if required packages are installed..."
+declare -a MISSING
+
+for pkg in "${LINUX_PREREQ[@]}"
+    do
+        echo "Installing '$pkg'..."
+        apt-get -y install $pkg
+        if [ $? -ne 0 ]; then
+            echo "Error installing system package '$pkg'"
+            exit 1
+        fi
+    done
+
+echo "All required packages have been installed!"
